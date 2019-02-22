b8dd267d12d88c9593bb9985f792ab8b867fb7d9
Jeanne Garcia <jeanne.ong.garcia@gmail.com>
2019-02-23 01:25:59 +0800

    View asset

--- assets/urls.py
+++ assets/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AssetTable, name='asset-table'),
-    path('view/', views.AssetView, name='asset-view'),
+    path('view/<int:asset_id>', views.AssetView, name='asset-view'),
     path('add/', views.AssetAdd, name='asset-add'),
     path('edit/', views.AssetEdit, name='asset-edit'),
     path('archive/', views.AssetArchive, name='asset-archive'),

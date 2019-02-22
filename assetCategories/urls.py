b6c791e8d524dc15cf53e70db565b3b9559c752c
Jeanne Garcia <jeanne.ong.garcia@gmail.com>
2019-02-23 01:39:03 +0800

    View asset category

--- assetCategories/urls.py
+++ assetCategories/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AssetCategoriesTable, name='asset-categories-table'),
-    path('view/', views.AssetCategoriesView, name='asset-categories-view'),
+    path('view/<int:category_id>', views.AssetCategoriesView, name='asset-categories-view'),
     path('add/', views.AssetCategoriesAdd, name='asset-categories-add'),
     path('edit/', views.AssetCategoriesEdit, name='asset-categories-edit'),
     path('archive/', views.AssetCategoriesArchive, name='asset-categories-archive'),

6be63e9a2aaebe53cfa871fb7713a18bb2744476
Jeanne Garcia <jeanne.ong.garcia@gmail.com>
2019-02-23 03:18:14 +0800

    Dashboard boxes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,15 @@
 # dashboard, views.py
-
+from datetime import timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from assets.models import Assets
 
 
 def Dashboard(request):
-    return render(request, 'dashboard.html', {'title': 'Dashboard'})
+    time_limit = timezone.now().date() - timedelta(days=30)
+    context = {
+        'asset_count': Assets.objects.all().count(),
+        'recently_added': Assets.objects.filter(date_added__gte=time_limit).count(),
+    }
+    return render(request, 'dashboard.html', context, {'title': 'Dashboard'})
